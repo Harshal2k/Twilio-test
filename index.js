5d91d5946c69bf11512da84a5c9053101180744a
Harshal Gosawi <harshalgosawi@gmail.com>
2024-02-07 23:54:10 +0530

    added dotenv support and created API to gather dialer input

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 const express = require('express')
+const dotenv = require('dotenv');
+dotenv.config();
+const urlencoded = require('body-parser').urlencoded;
 const app = express()
-const port = 3000
+app.use(urlencoded({ extended: false }));
+const port = process.env.PORT;
 
-const accountSid = "AC4946f7879ba9a71ec12bc85287e29b98";
-const authToken = "cd3217aa63e8f4d057ae328498a75027";
+const accountSid = process.env.ACCOUNT_SID;
+const authToken = process.env.AUTH_TOKEN;
 const client = require('twilio')(accountSid, authToken);
 const twilio = require('twilio');
 const { VoiceResponse } = twilio.twiml;
 
+const connectedNumbers = new Set();
 
 app.get('/call', (req, res) => {
     client.calls.create({
@@ -18,9 +23,14 @@ app.get('/call', (req, res) => {
         .then(call => res.send(call));
 })
 
-app.get('/availablePhoneNumbers', (req, res) => {
-    client.incomingPhoneNumbers.list({ status: 'active' })
-        .then(hosted_number_order => { console.log(hosted_number_order); res.send(hosted_number_order) });
+app.get('/availablePhoneNumbers', async (req, res) => {
+    let numbers = await client.incomingPhoneNumbers.list({ status: 'active' });
+    console.log({ numbers });
+    res.send(numbers);
+
+    // client.incomingPhoneNumbers.list({ status: 'active' })
+    //     .then(hosted_number_order => { console.log(hosted_number_order); res.send(hosted_number_order) })
+    //     .catch((err) => res.send(err))
 })
 
 app.get('/incoming-call', (req, res) => {
@@ -28,11 +38,51 @@ app.get('/incoming-call', (req, res) => {
     console.log("-------------------------")
     console.log({ from: req.body });
     console.log({ query: req.query });
-    twiml.dial({ callerId: '+19134236245' }, '+919359192032'); // Replace with the destination phone number
+    twiml.dial({ callerId: '+19134236245' }, '+919359192032');
 
     res.type('text/xml');
     res.send(twiml.toString());
 });
+
+app.post('/getherInput', (req, res) => {
+    const twiml = new VoiceResponse();
+
+    /** helper function to set up a <Gather> */
+    function gather() {
+        const gatherNode = twiml.gather({ numDigits: 1 });
+        gatherNode.say('For sales, press 1. For support, press 2.');
+
+        // If the user doesn't enter input, loop
+        twiml.redirect('/voice');
+    }
+
+    // If the user entered digits, process their request
+    if (req.body.Digits) {
+        switch (req.body.Digits) {
+            case '1':
+                twiml.say('You selected sales. Good for you!');
+                console.log('You selected sales. Good for you!')
+                break;
+            case '2':
+                twiml.say('You need support. We will help!');
+                console.log('You need support. We will help!');
+                break;
+            default:
+                twiml.say("Sorry, I don't understand that choice.");
+                console.log("Sorry, I don't understand that choice.");
+                twiml.pause();
+                gather();
+                break;
+        }
+    } else {
+        // If no input was sent, use the <Gather> verb to collect user input
+        gather();
+    }
+
+    // Render the response as XML in reply to the webhook request
+    res.type('text/xml');
+    res.send(twiml.toString());
+})
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
